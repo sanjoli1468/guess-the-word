f68055d57d6ebc26401432945b3d372037a40cca
sanjoli1468 <81970448+sanjoli1468@users.noreply.github.com>
2021-11-19 17:27:42 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,79 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + ":";
+document.getElementById('player2_name').innerHTML = player2_name + ":";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_question").innerHTML = "Question Turn -" + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+
+function send(){
+    get_the_word = document.getElementById("word").value;
+    word = get_the_word.toLowerCase();
+    console.log("word in lowercase = " + word);
+
+    charAt1 = word.charAt(1);
+    console.log("charAt second position =" + charAt1);
+
+    length = Math.floor(word.length/2);
+    charAt2 = word.charAt(length);
+    console.log(charAt2);
+
+    length_minus = word.length - 1;
+    charAt3 = word.charAt(length_minus);
+    console.log(charAt3);
+
+    remove_1 = word.replace(charAt1, "_");
+    console.log(remove_1);
+    remove_2 = remove_1.replace(charAt2, "_");
+    remove_3 = remove_2.replace(charAt3, "_");
+
+    question_word = "<h4 id='word_display'> Q. "+remove_3+"</h4>";
+    input_box = "<br> Answer: <input type='text' id='input_check_box'>";
+    check_button = "<br> <br> <button class= 'btn btn-info' onclick='check()'> check </button>";
+    row = question_word + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("word").value = "";
+}
+question_turn = "player1";
+answer_turn = "player2";
+
+function check(){
+get_answer = document.getElementById("input_check_box").value;
+answer = get_answer.toLowerCase();
+
+if(answer == word){
+    if(answer_turn == "player1"){
+        player1_score = player1_score + 1;
+        document.getElementById("player1_score").innerHTML = player1_score;
+    }
+   else{
+       player2_score = player2_score + 1;
+       document.getElementById("player2_score").innerHTML = player2_score;
+   }
+}
+if(question_turn =="player1"){
+    question_turn = "player2"
+    document.getElementById("player_question").innerHTML = "question turn - " + player2_name;
+}
+else{
+    question_turn ="player1"
+    document.getElementById("player_question").innerHTML = "question turn - " + player1_name;
+}
+
+if(answer_turn == "player1"){
+    answer_turn = "player2";
+    document.getElementById("player_answer").innerHTML = "answer turn - " + player2_name;
+}
+else{
+    answer_turn = "player1"
+    document.getElementById("player_answer").innerHTML = "answer turn - " + player1_name;
+}
+document.getElementById('output').innerHTML = "";
+}
